befef17cd1635b31b3be558aef95a28d2f6181fe
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-09-01 16:48:06 +0100

    starting point

---
+++ README.md
@@ -0,0 +1,3 @@
+This: https://expressjs.com/en/starter/hello-world.html
+Then you change it to .ts and ts-node and voila
+And then you can use nodemon instead of ts-node
---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+let counter = 0;
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/counter', (req, res) => {
+  res.send(counter);
+})
+
+app.post('/counter', (req, res) => {
+  counter += 1;
+  res.send(counter);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
