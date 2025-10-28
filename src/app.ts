cac254ff99baea13c7843fc79e90e3d3bd5245c3
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2025-10-28 15:21:53 +0000

    include cors in backend

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+const corsOptions = {
+  origin: '*'
+}
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({ res: 'Hello World!' })
 })
 
 app.listen(port, () => {
