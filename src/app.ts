c9c81d91adf78b698dca3ebf4496556505b631c9
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2025-10-28 15:22:56 +0000

    add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors";
 
+// create express app & choose port
 const app = express();
 const port = 3000;
 
+// setup CORS to allow requests from any origin
 const corsOptions = {
   origin: '*'
 }
-
 app.use(cors(corsOptions));
 
+// hello world response
 app.get('/', (req, res) => {
   res.send({ res: 'Hello World!' })
 })
 
+// start server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
