888680f916240a5f9e964330a33e61e06d840687
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-10-30 07:15:26 +0000

    add body parser

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 // create express app & choose port
 const app = express();
@@ -10,6 +11,10 @@ const corsOptions = {
   origin: '*'
 }
 app.use(cors(corsOptions));
+
+// setup JSON & body Parser
+app.use(bodyParser.json()); // parse json requests
+app.use(bodyParser.urlencoded({ extended: false })); // parse url encoded requests
 
 // hello world response
 app.get('/', (req, res) => {
